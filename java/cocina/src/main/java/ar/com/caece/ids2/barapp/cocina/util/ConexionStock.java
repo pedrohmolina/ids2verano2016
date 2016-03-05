d159249b7f07f0f6f6b4cb15457335365c0b28b0
Ruben Impa <rcondori@B-Vision.local>
2016-03-05 18:51:03 -0300

    issue 22

---
+++ java/cocina/src/main/java/ar/com/caece/ids2/barapp/cocina/util/ConexionStock.java
@@ -0,0 +1,8 @@
+package ar.com.caece.ids2.barapp.cocina.util;
+
+public class ConexionStock {
+	
+	public final static int STATUS_CODE_STOCK_OK = 0;
+	public final static String URL_STOCK = "http://localhost:8080/realizarPedido";
+
+}
